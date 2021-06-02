923e1900317124faca591ffdfb949470ee3a9812
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-02 22:48:19 +0530

    add : register page ui

---
+++ src/pages/register.tsx
@@ -0,0 +1,58 @@
+/** @format */
+
+import React from "react";
+import { Field, Form, Formik } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+  Box,
+} from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              label="Username"
+              placeholder="Enter Username"
+              type="text"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                label="Password"
+                placeholder="Enter password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variant="solid"
+              colorScheme="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+export default Register;
