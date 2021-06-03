8fd8eadbc2f5f45f99b694b2708e08977ba415a6
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-03 22:32:17 +0530

    add : me query & fetch logged in user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,19 @@
 import React from "react";
 import NextLink from "next/link";
 import { Box, Flex, Link } from "@chakra-ui/layout";
+import { useMeQuery } from "../generated/graphql";
+import { Button } from "@chakra-ui/button";
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
-  return (
-    <Flex bg="tomato">
-      <Box p={4} ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link color="white" mr={3}>
             Login
@@ -18,6 +24,24 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
         <NextLink href="/register">
           <Link color="white">Register</Link>
         </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box color="white" mr={3}>
+          {data.me.username}
+        </Box>
+        <Button variant="link" color="white">
+          Logout
+        </Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="tomato">
+      <Box p={4} ml={"auto"}>
+        {body}
       </Box>
     </Flex>
   );
